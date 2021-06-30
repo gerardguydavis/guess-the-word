f4e6c8a4815473ce7d771610f7c2beb760ed67b3
Gerard Davis <gerardguydavis@outlook.com>
2021-06-30 15:58:08 -0400

    Add functions to validate user guess and log user guesses

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@ const remainNum = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
+//To replace secret word characters with dots
 const dots = function (word) {
     const dotSpots = [];
     for (let letter of word) {
@@ -18,9 +20,38 @@ const dots = function (word) {
 
 dots(word);
 
+//To submit user guess
 guessButton.addEventListener("click", function(e) {
     e.preventDefault();
     const guessInput = userGuess.value;
     console.log(guessInput);
     userGuess.value = "";
-})
+    message.innerText = "";
+    validateGuess(guessInput);
+    makeGuess(guessInput);
+});
+
+const validateGuess = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "You have to guess something!";
+    }
+    else if (input.length > 1) {
+        message.innerText = "You can only guess one letter at a time!";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "We're only playing with letters here, ma'am.";
+    }
+    else {
+        return input;
+    }
+}
+
+const makeGuess = function (letter) {
+    letter.toUpperCase();
+    if (guessedLetters.includes(letter)) {
+        message.innerText = "You already guessed that letter!";
+    } else {
+        guessedLetters.push(letter);
+    }
+    console.log(guessedLetters);
+}
